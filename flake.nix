3e5ff7c7cfffd1440555ad2e1ebb047ea584f325
BeedPro <beed.epistle@outlook.com>
2026-07-16 11:00:35 +0100

    fix: invoke nixgl launcher wrappers

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
                 fi
 
                 export PYTHONPATH="$PWD/src''${PYTHONPATH:+:$PYTHONPATH}"
-                exec nix shell --impure ${nixgl}#${nixglPackage} --command python -m poomer "$@"
+                exec nix shell --impure ${nixgl}#${nixglPackage} --command nixGL python -m poomer "$@"
               '';
             };
           poomerNixglNvidia = pkgs.writeShellApplication {
@@ -65,7 +65,7 @@
               fi
 
               export PYTHONPATH="$PWD/src''${PYTHONPATH:+:$PYTHONPATH}"
-              exec nix shell --impure --expr "let pkgs = import ${nixgl.inputs.nixpkgs} { system = builtins.currentSystem; config.allowUnfree = true; }; nixgl = import ${nixgl} { inherit pkgs; nvidiaVersion = \"$nvidia_version\"; }; in nixgl.nixGLNvidia" --command python -m poomer "$@"
+              exec nix shell --impure --expr "let pkgs = import ${nixgl.inputs.nixpkgs} { system = builtins.currentSystem; config.allowUnfree = true; }; nixgl = import ${nixgl} { inherit pkgs; nvidiaVersion = \"$nvidia_version\"; }; in nixgl.nixGLNvidia" --command "nixGLNvidia-$nvidia_version" python -m poomer "$@"
             '';
           };
           poomerNixgl = makeNixglPoomer "poomer-nixgl" "nixGLDefault";
